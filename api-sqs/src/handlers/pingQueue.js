a6fc6d4c6eb5e73053e170ba08c0a8e203900bd3
Sufiyan Rahmat <sufiyan@incube8.sg>
2019-09-19 14:22:28 +0800

    Replace with slsrun/normalizeHttpRequest middleware

--- api-sqs/src/handlers/pingQueue.js
+++ api-sqs/src/handlers/pingQueue.js
@@ -1,6 +1,6 @@
 import middy from 'middy';
+import { normalizeHttpRequest } from 'slsrun';
 
-import normalizeRequest from 'Middlewares/normalizeRequest';
 import errorHandler from 'Middlewares/errorHandler';
 import responseHandler from 'Middlewares/responseHandler';
 
@@ -14,6 +14,6 @@ const originalHandler = event => {
 export const handler = middy(originalHandler);
 
 handler
-  .use(normalizeRequest())
+  .use(normalizeHttpRequest())
   .use(responseHandler())
   .use(errorHandler());
